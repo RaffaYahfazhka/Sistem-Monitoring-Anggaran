9c8f58c1597429e2c434503c51f1ffbd1aaaf7d9
Raffa Yahfazhka <mraffayahfazhka@gmail.com>
2022-07-22 14:11:14 +0700

    enter

--- pages/user/rekaphpp.php
+++ pages/user/rekaphpp.php
@@ -194,13 +194,6 @@ if (isset($_POST['simpan'])) {
                         </a>
                     </li>
 
-                    <li class="search-box">
-                        <a href="registrasi.php">
-                            <i class='bx bx-user -alt icon' ></i>
-                            <span class="text nav-text"> Data User</span>
-                        </a>
-                    </li>
-
                 </ul>
             </div>
 
@@ -253,13 +246,6 @@ if (isset($_POST['simpan'])) {
                     <a href="">
                         <span style="margin-left: 0%; text-align: left;"><?php echo $_SESSION['name'] ?></span>
                       </a>
-
-                  <a href="inputdatauser.php">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" style="margin-bottom: 2.4px;" fill="currentColor" class="bi bi-person-plus-fill" viewBox="0 0 16 16">
-                      <path d="M1 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/><path fill-rule="evenodd" d="M13.5 5a.5.5 0 0 1 .5.5V7h1.5a.5.5 0 0 1 0 1H14v1.5a.5.5 0 0 1-1 0V8h-1.5a.5.5 0 0 1 0-1H13V5.5a.5.5 0 0 1 .5-.5z"/>
-                    </svg>
-                    <span style="margin-left: 12%;">Add User</span>
-                  </a>
 
                   <a href="../../includes/logout.php">
                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" style="margin-bottom: 2.4px;" fill="currentColor" class="bi bi-box-arrow-right" viewBox="0 0 16 16">
